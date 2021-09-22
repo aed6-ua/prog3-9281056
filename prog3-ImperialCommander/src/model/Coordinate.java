2dc7219e3f5fade5fc92f694f4867e3709960eba
Edu <aed6@alu.ua.es>
2021-09-22 22:44:28 +0200

    Added hashCode to Coordinate

--- prog3-ImperialCommander/src/model/Coordinate.java
+++ prog3-ImperialCommander/src/model/Coordinate.java
@@ -22,13 +22,31 @@ public class Coordinate {
 		return y;
 	}
 	
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
 	public boolean equals(Object obj) {
-			if (obj == this) return true;
-			if (obj == null) return false;
-			if (!(obj instanceof Coordinate)) return false;
-			Coordinate elotro = (Coordinate)obj;
-			if (elotro.x==this.x && elotro.y==this.y) return true;
-			else return false;
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Coordinate other = (Coordinate) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
 	}
 	
 	public String toString() {
