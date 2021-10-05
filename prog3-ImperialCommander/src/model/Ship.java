d030c3d747cf71b61001183cba275a3ccd3445ed
Edu <aed6@alu.ua.es>
2021-10-05 17:02:17 +0200

    Ship | showFleet, myFleet, toString

--- prog3-ImperialCommander/src/model/Ship.java
+++ prog3-ImperialCommander/src/model/Ship.java
@@ -125,6 +125,69 @@ public class Ship {
 	public void purgeFleet() {
 		fleet.removeIf(n -> !(n.isDestroyed()));
 	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String showFleet() {
+		if (this.fleet.isEmpty()) {
+			String r = new String("");
+			return r;
+			}
+		else {
+			StringBuilder sb = new StringBuilder();
+			for(Fighter f: fleet) {
+				sb.append(f.toString());
+				if (f.isDestroyed()) sb.append("(X)");
+				sb.append("\n");
+			}
+			String s = sb.toString();
+			return s;
+		}
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String myFleet() {
+		if (this.fleet.isEmpty()) {
+			String r = new String("");
+			return r;
+			}
+		else {
+			StringBuilder sb = new StringBuilder();
+			Map<String, Integer> fighterMap = new HashMap<>();
+			for (Fighter f: this.fleet) {
+				Integer n = fighterMap.get(f.getType());
+				if (n==null) {
+					n=0;
+				}
+				
+				fighterMap.put(f.getType(), n+1);
+			}
+			fighterMap.forEach((f,n) -> { 	sb.append(n);
+											sb.append("/");
+											sb.append(f);
+											sb.append(":");
+										});
+			sb.deleteCharAt(sb.length() - 1);
+			String s = sb.toString();
+			return s;
+		}
+	}
 	
-	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Ship [");
+		sb.append(this.name);
+		sb.append(" ");
+		sb.append(this.wins);
+		sb.append("/");
+		sb.append(this.losses);
+		sb.append("] ");
+		sb.append(this.myFleet());
+		String s = sb.toString();
+		return s;
+	}
 }
